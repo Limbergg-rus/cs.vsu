028d9f0ae5f54e91e1dbf2cb235ae864782c57b5
Limbergg-rus <aslanovrenat7@gmail.com>
2023-11-05 16:20:30 +0300

    task8

---
+++ task8/InputAndOutput.java
@@ -0,0 +1,67 @@
+package ru.vsu.cs.aslanovrenat.oldtasks.task8;
+
+import java.io.*;
+import java.util.*;
+
+public class InputAndOutput {
+    // Считывание одномерного массива из файла
+    public static int[] inputArray(String fileName) throws FileNotFoundException {
+        List<Integer> line = new ArrayList<>();
+        Scanner sc = new Scanner(new File(fileName));
+        sc.useDelimiter("(\\s|[;,])+").useLocale(Locale.ROOT);
+        while (sc.hasNextInt()) {
+            line.add(sc.nextInt());
+        }
+        Integer[] arr = line.toArray(new Integer[0]);
+        return toPrimitiveIntegerSingle(arr);
+    }
+
+    // Вывод двумерного массива в файл
+    public static void outputArray(String fileName, int[][] array) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            for (int[] i : array) {
+                writer.write(Arrays.toString(i) + ",\n");
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // Перевод в int[] одномерного массива
+    private static int[] toPrimitiveIntegerSingle(Integer[] arr) {
+        int rows = arr.length;
+        int[] result = new int[rows];
+        for (int i = 0; i < rows; i++) {
+            result[i] = arr[i];
+        }
+        if (result[0] < 1 || result[0] > 12 || result[1] < 0) {
+            System.out.println("Введены некорректные данные");
+            System.exit(0);
+        }
+        return result;
+    }
+}
+
+
+// Перевод в int[][] двумерного массива
+/* Считывание двумерного массива
+    private static int[][] inputArray(String fileName) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(fileName));
+        sc.useDelimiter("\n").useLocale(Locale.ROOT);
+        List<List> totalList = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            Scanner line = new Scanner(sc.nextLine());
+            line.useDelimiter("(\\s|[;,])+").useLocale(Locale.ROOT);
+            List<Integer> list = new ArrayList<>();
+            while (line.hasNextInt()) {
+                list.add(line.nextInt());
+            }
+            totalList.add(list);
+            line.close();
+        }
+        sc.close();
+        return listTo2DList(totalList);
+    }
+
+ */
---
+++ task8/Window.java
@@ -0,0 +1,113 @@
+package ru.vsu.cs.aslanovrenat.oldtasks.task8;
+
+import ru.vsu.cs.aslanovrenat.task8.Utils.ArrayUtils;
+import ru.vsu.cs.aslanovrenat.task8.Utils.JTableUtils;
+import ru.vsu.cs.aslanovrenat.task8.Utils.SwingUtils;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+
+public class Window extends JFrame {
+    private JButton buttonLoadInputFromFile;
+    private JButton buttonSaveInputInfoFile;
+    private JButton outputFileButton;
+    private JTable tableInput;
+    private JTable tableOutput;
+    private JPanel panelMain;
+    private JFileChooser fileChooserOpen;
+    private JFileChooser fileChooserSave;
+    private JMenuBar menuBarMain;
+
+    public Window() {
+        this.setTitle("FrameMain");
+        this.setContentPane(panelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+
+        JTableUtils.initJTableForArray(tableInput, 80, true, true, true, true);
+        JTableUtils.initJTableForArray(tableOutput, 40, true, true, true, true);
+        //tableOutput.setEnabled(false);
+        tableInput.setRowHeight(25);
+        tableOutput.setRowHeight(25);
+
+        fileChooserOpen = new JFileChooser();
+        fileChooserSave = new JFileChooser();
+        fileChooserOpen.setCurrentDirectory(new File("."));
+        fileChooserSave.setCurrentDirectory(new File("."));
+        FileFilter filter = new FileNameExtensionFilter("Text files", "txt");
+        fileChooserOpen.addChoosableFileFilter(filter);
+        fileChooserSave.addChoosableFileFilter(filter);
+
+        fileChooserSave.setAcceptAllFileFilterUsed(false);
+        fileChooserSave.setDialogType(JFileChooser.SAVE_DIALOG);
+        fileChooserSave.setApproveButtonText("Save");
+
+        menuBarMain = new JMenuBar();
+        setJMenuBar(menuBarMain);
+
+
+        JTableUtils.writeArrayToJTable(tableInput, new int[][]{
+                {11, 2023}
+        });
+        this.pack();
+        setSize(700, 540); // Размер окна
+
+        //Кнопка ввода данных из файла
+        buttonLoadInputFromFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    if (fileChooserOpen.showOpenDialog(panelMain) == JFileChooser.APPROVE_OPTION) {
+                        int[][] arr = ArrayUtils.readIntArray2FromFile(fileChooserOpen.getSelectedFile().getPath());
+                        JTableUtils.writeArrayToJTable(tableInput, arr);
+                    }
+                } catch (Exception e) {
+                    SwingUtils.showErrorMessageBox(e);
+                }
+            }
+        });
+
+        // Кнопка выполнение вывода калькулятора
+        outputFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    int[] matrix = JTableUtils.readIntArrayFromJTable(tableInput);
+                    if (matrix[0] < 1 || matrix[0] > 12 || matrix[1] < 0) {
+                        JOptionPane.showMessageDialog(null, "Введены некорректные данные");
+                    } else {
+                        JTableUtils.writeArrayToJTable(tableOutput, Algoritmes.calendarPrinter(matrix));
+                    }
+                } catch (Exception e) {
+                    SwingUtils.showErrorMessageBox(e);
+                }
+            }
+        });
+
+
+        //Кнопка вывода в файл результата
+        buttonSaveInputInfoFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    if (fileChooserSave.showSaveDialog(panelMain) == JFileChooser.APPROVE_OPTION) {
+                        int[][] matrix = JTableUtils.readIntMatrixFromJTable(tableOutput);
+                        String file = fileChooserSave.getSelectedFile().getPath();
+                        if (!file.toLowerCase().endsWith(".txt")) {
+                            file += ".txt";
+                        }
+                        ArrayUtils.writeArrayToFile(file, matrix);
+                    }
+                } catch (Exception e) {
+                    SwingUtils.showErrorMessageBox(e);
+                }
+            }
+        });
+    }
+
+}
